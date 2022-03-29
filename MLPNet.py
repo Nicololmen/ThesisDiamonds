285bfb69af2e725776281211cc2a2cc5a2e9cd15
Nicololmen <schepers.milan@gmail.com>
2022-03-29 19:13:52 +0200

    Working code to start from

--- MLPNet.py
+++ MLPNet.py
@@ -7,7 +7,7 @@ Created on Fri Feb 25 14:24:47 2022
 
 
 import torch.nn as nn
-
+from torchsummary import summary
 
 
 class Net(nn.Module):
@@ -17,9 +17,10 @@ class Net(nn.Module):
     layers = []
     for index, (layer_in, layer_out) in enumerate(zip(architecture_lijst[0:], architecture_lijst[1:])):
       layers.append(nn.Linear(int(layer_in), int(layer_out)))
-      if (index != len(architecture_lijst) - 1):
+      if (index < len(architecture_lijst) - 2):
         layers.append(nn.ReLU())
     self.model = nn.Sequential(*layers)
+    print(summary(self.model, (0, 0, 641)))
 
   def forward(self, input):
     return self.model(input)
--- Training.py
+++ Training.py
@@ -13,7 +13,7 @@ import torch.nn.functional as F
 import pandas as pd
 import MLPNet
 import CustomDataSet
-from HulpFunctions.evaluation_metrics import calculate_accuracy
+from HulpFunctions.evaluation_metrics import calc_ap
 import wandb
 import argparse
 from tqdm import tqdm
@@ -26,13 +26,17 @@ def run_val_epoch(model, val_loader, epoch_index, device='cpu'):
     # Put model in eval mode
     model.eval()
 
-    labels, predictions = run_eval_step(model, val_loader, device)
+    predictions, labels = run_eval_step(model, val_loader, device)
 
-    accuracy = calculate_accuracy(torch.Tensor(labels), torch.Tensor(predictions))
+    sim_mat = F.softmax(predictions, dim=1)
 
-    # Log the evaluation accuracy
-    wandb.log({"epoch": epoch_index,
-               "evaluation accuracy": accuracy})
+    uniq_labels = np.array([0, 1, 2, 3, 4, 5])
+
+    for label in uniq_labels:
+        ap = calc_ap(label, sim_mat, uniq_labels, labels)
+        # Log the evaluation accuracy
+        wandb.log({"epoch": epoch_index + 1,
+                   f"avarage precision {label}": ap})
 
 
 def run_eval_step(model, data_loader, device='cpu'):
@@ -57,9 +61,10 @@ def run_eval_step(model, data_loader, device='cpu'):
 # passes batch through model and calculates and returns cross_entropy loss
 def run_train_step(model, batch, num_classes):
     inputs, labels = batch
+
     outputs = model(inputs)
+
     one_hot_targets = F.one_hot(labels, num_classes)
-    print(one_hot_targets)
     return F.binary_cross_entropy_with_logits(
         outputs,
         one_hot_targets.float()
@@ -87,7 +92,7 @@ def run_train_epoch(model, train_loader, optimizer, epoch_index, device='cpu'):
         optimizer.step()
 
         # Log the training loss
-        wandb.log({"epoch": epoch_index,
+        wandb.log({"epoch": epoch_index + 1,
             "training loss": loss})
 
 
@@ -97,7 +102,7 @@ def run_training(model, optimizer, train_loader, val_loader, num_epochs=10):
 
     for epoch_index in tqdm(range(num_epochs), desc='Epoch'):
         run_train_epoch(model, train_loader, optimizer, epoch_index, device)
-       #run_val_epoch(model, val_loader, epoch_index, device)
+        run_val_epoch(model, val_loader, epoch_index, device)
 
 def main():
     
@@ -110,7 +115,7 @@ def main():
     parser.add_argument("--epochs", help='Supply amount of epochs for training, default value=25', default='25')
     parser.add_argument("--batch_size", help='Supply batch_size for training loader, default value=32', default='32')
     parser.add_argument("--num_workers", help='Supply num_workers for training loader, default value=1', default='1')
-    parser.add_argument("--num_nodes_per_layer", help="Give model architecture eg 512-256-128-6", default="512-256-64-6")
+    parser.add_argument("--num_nodes_per_layer", help="Give model architecture eg 512-256-128-6", default="641-16-6")
     parser.add_argument("--P_or_R", help="1 for polished 0 for rough", default="0")
 
     args=parser.parse_args()
@@ -120,9 +125,10 @@ def main():
     Headers = Data.columns
     if(int(args.P_or_R)):
         drop_headers = ['Lot', 'SCTF_CLARITY', 'PolishedPicture', 'IntegrationTimePolished', 'SCTF_FLUO', 'GIAColor', 'FileName', 'PolishedFile', 'wp']
+        dst = 'Polished'
     if(not(int(args.P_or_R))):
         drop_headers = ['Lot', 'MR_CLARITY', 'RoughPicture', 'IntegrationTimeRough', 'MR_FLUO', 'GIAColor', 'FileName', 'wp', 'wr']
-
+        dst = 'Rough'
     # Get training Data
     data_set = CustomDataSet.CustomDataSet(args.data_set, 1, 9000, Headers, drop_headers)
     train_loader = torch.utils.data.DataLoader(data_set, batch_size=int(args.batch_size), shuffle=True, num_workers=int(args.num_workers))
@@ -132,7 +138,7 @@ def main():
 
     # Set-up weight and biases
     wandb.require(experiment="service")
-    wandb.init(name=f'training_run {args.num_nodes_per_layer} {datetime.datetime.now().date()}',
+    wandb.init(name=f'training_run {args.num_nodes_per_layer} {datetime.datetime.now().date()} {dst}',
                project='Thesis Diamonds')
     wandb.config.lr=float(args.learning_rate)
     wandb.config.wd=float(args.weight_decay)
